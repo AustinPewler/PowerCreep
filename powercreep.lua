c47b6b6fa0e90a8103946f48e3af43bd4c0a058a
Dogwearingdurag <112187047+AustinPewler@users.noreply.github.com>
2025-05-11 15:10:36 -0400

    Update powercreep.lua
    Raiden257 on Nexus Contribution for crashes due to high FPS

--- powercreep.lua
+++ powercreep.lua
@@ -44,30 +44,43 @@ SMODS.Joker{
         return {vars = {center.ability.extra.Xmult}}
     end,
     calculate = function(self, card, context)
-        if context.setting_blind then
-            G.E_MANAGER:add_event(Event({func = function()
-                ease_hands_played(-G.GAME.current_round.discards_left, nil, true)
-            return true end }))
-        end
-        if context.joker_main then
-            return {
-                Xmult =1.5
-            }
-        end
-        if context.setting_blind then
-            event_added = false
-        end
-        local counter = #SMODS.find_card('j_pow_powercreep')
-        if not event_added and G.GAME.blind.boss and (G.GAME.chips / G.GAME.blind.chips >= 3) then
-            for i = 1, counter do
-                local new_card = create_card('powercreep', G.jokers, nil, nil, nil, nil, 'j_pow_powercreep')
-                new_card:set_edition({ negative = true }, true)
-                new_card:add_to_deck()
-                G.jokers:emplace(new_card)
-            end
-            event_added = true -- Set the flag to true after adding the event
-        end
-    end,
+  -- if card.edition and card.edition.negative then return end
+
+  if context.setting_blind then
+G.E_MANAGER:add_event(Event({func = function()
+ease_hands_played(-G.GAME.current_round.discards_left, nil, true)
+return true end }))
+  end
+  if context.joker_main then
+return {
+Xmult =1.5
+}
+  end
+  if context.setting_blind then
+event_added = false
+  end
+
+  -- count all powercreep jokers
+  local counter = #SMODS.find_card('j_pow_powercreep')
+ 
+  if not event_added and G.GAME.blind.boss and (G.GAME.chips / G.GAME.blind.chips >= 3) then
+-- creates a new event for the joker generation,
+-- this occurs once after the boss blind is beaten with more than 3x blind size to ensure no random joker creation.
+G.E_MANAGER:add_event(Event({ func = function()
+for i = 1, counter do
+  -- original here was create 'powercreep' joker cards, I've changed it to 'j_pow_powercreep',
+  -- no idea if this changes anything but it works so far
+  local new_card = create_card('j_pow_powercreep', G.jokers, nil, nil, nil, nil, 'j_pow_powercreep')
+  new_card:set_edition({ negative = true }, true)
+  new_card:add_to_deck()
+  G.jokers:emplace(new_card)
+end
+return true
+end }))
+
+event_added = true
+  end
+end,
     in_pool = function(self, wawa, wawa2)
         return true
     end,
